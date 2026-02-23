cee7c7e1d5e61156b63991cce0a75e22a6b79289
Szarps <szarps@hotmail.com>
2026-02-23 17:21:59 -0300

    First commit, skeleton structure

---
+++ newproject.py
@@ -0,0 +1,67 @@
+from pathlib import Path
+
+Path
+
+folders: tuple = ("config", "data", "docs", "public", "scripts", "src", "tests", ".github")
+
+sub_folders: tuple = ("app", "application", "domain", "e2e", "hooks", "integration", "lib", "style", "types", "unit", "infrastructure", "interfaces", "components", "ui", "layout", "utils", "features")
+
+projects: dict = {"Minimal / Script / Tiny CLI / Jupyter-heavy", "Frontend / React / Next.js / Vite", "Standard backend / full-stack"}
+
+
+def mini(name: String):
+    print()
+    # create folder for project passed by argument
+    # create following folders
+    # data, docs, scripts, src, tests
+    #    create following sub-folders
+    #    features, utils
+    return print("Job done!")
+
+
+def backend(name: String):
+    print()
+    # create folder for project passed by argument
+    # create following folders
+    # src, tests, config, docs, scripts, .github
+    #     sub_folders
+    #     src -> application, domain, infrastructure, interfaces
+    #     tests -> e2e, integration, unit
+
+
+def frontend(name: String):
+    print()
+    # folders
+    # src, public, tests, docs, .github
+    #     sub_folders
+    #     src -> app, components, lib, hooks, styles, types
+    #     components -> ui, features, layout
+
+
+create = True
+
+
+def main():
+    print("Hello! This is a simple script to establish the workspace for your next project.")
+    while create is True:
+        x = input()
+
+        match x:
+            case _:
+                print("Please input a valid parameter following the next format:\n<project_name> <project_type>\n")
+                print("Here's a list of options for types of projects:")
+                print(f"{[v for v in projects]}")
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
